87b36a15538bed14546d8081e7f7c36e0b96ba41
Rustamov-Zarrrukh <zarruhzak@gmail.com>
2025-02-20 09:28:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// №1
+let number = 1
+
+
+for (let i = 1 ; i < 100; i++){
+
+  console.log(` ${number} + ${i} = ${number+i}`)
+
+}
+
+
+// №2
+
+let names = ["Илья", "Амаль", "Ахмад", "Фаррух", "Шерик"]
+
+for (let i = 0; i < 5; i++) {
+  console.log(`Привет ${names[i]}`);
+
+}
+
+// №3
+
+let txt = "Text"
+for (let i = 0; i < txt.length; i++) {
+  console.log(txt[i]);
+
+
+}
